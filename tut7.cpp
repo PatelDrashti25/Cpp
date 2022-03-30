95d4ce90544c70abcb2bfc85aa2e55688f71de3d
PatelDrashti25 <98554551+PatelDrashti25@users.noreply.github.com>
2022-03-31 00:00:38 +0530

    Create tut7.cpp

---
+++ tut7.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    /*Loops in C++:
+    There are three types of loops in C++:
+        1.For Loop
+        2.While Loop
+        3.do-while Loop
+    */
+   /* For loop in C++ */
+   //int i=1;
+   //cout<<i;
+   //i++;
+
+   //Syntax for For Loop
+   //   for(initialization; condition; updation)
+   // {
+   //   loop body(C++ code);
+   // }
+
+   //    for (int i = 0; i < 40; i++)
+   //    {
+   //        /* code */
+   //        cout<<i<<endl;
+   //    }
+   
+   /*While lopp in C++*/
+   // Syntax:
+   // while (condition)
+   //{
+   //    C++ statements;   
+   //}
+
+   // Printing 1 to 40 using while loop
+   //    int i = 30;
+   //    while (i<=40)
+   //    {
+   //       cout<<i<<endl;
+   //       i++;
+   //    }
+   
+   /*do-While lopp in C++*/
+   // Syntax:
+   // do
+   //{
+   //    C++ statements;   
+   //} while (condition)
+
+   // Printing 1 to 40 using do-while loop
+   //int i = 10;
+   //do{
+   //  cout<<i<<endl;
+   //    i++;
+   //}  while (false);
+
+    return 0;
+}
