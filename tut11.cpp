86e76839a348adc1292963b334cb6277d3c77a5d
PatelDrashti25 <98554551+PatelDrashti25@users.noreply.github.com>
2022-04-12 16:28:40 +0530

    Update tut11.cpp

--- tut11.cpp
+++ tut11.cpp
@@ -1,45 +1,38 @@
 #include<iostream>
 using namespace std;
 
+typedef struct employee
+{
+    /* data */
+    int eid;
+    char favchar;
+    float salary;
+} ep;
+
+union money
+{
+    /* data */
+    int rice; //4 byte
+    char car; //1  byte
+    float pounds; //4 byte
+};
+
+
 int main(){
-    
-    int marks[] = {23, 45, 56, 89};
 
-    int mathmarks[4];
-    mathmarks[0] = 2278;
-    mathmarks[1] = 78;
-    mathmarks[2] = 6278;
-    mathmarks[3] = 378;
-
-    cout<<"These are math marks"<<endl;
-    cout<<mathmarks[0]<<endl;
-    cout<<mathmarks[1]<<endl;
-    cout<<mathmarks[2]<<endl;
-    cout<<mathmarks[3]<<endl;
-
-    //You can change the value of an array
-    marks[2] = 455;
-    cout<<"These are marks"<<endl;
-    // cout<<marks[0]<<endl;
-    // cout<<marks[1]<<endl;
-    // cout<<marks[2]<<endl;
-    // cout<<marks[3]<<endl;
+    enum meal{ breakfast, lunch, dinner};
+    meal m1 = breakfast;
+    cout<<(m1==2);
+    // union money m1;
+    // m1.rice = 34;
+    // m1.car = 'c';
+    // cout<<m1.car;
+    // harry.eid = 1;
+    // harry.favchar = 'c';
+    // harry.salary = 1200000;
+    // cout<<"The value is "<<harry.eid<<endl;
+    // cout<<"The value is "<<harry.favchar<<endl;
+    // cout<<"The value is "<<harry.salary<<endl;
     
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     cout<<"The value of marks "<<i<<" is "<<marks[i]<<endl;  
-    // }
-
-    //Pointers and arrays
-    int* p = marks;
-    cout<<*(p++)<<endl;
-    cout<<*(++p)<<endl;
-    // cout<<"The value of *p is "<<*p<<endl;
-    // cout<<"The value of *(p+1) is "<<*(p+1)<<endl;
-    // cout<<"The value of *(p+2) is "<<*(p+2)<<endl;
-    // cout<<"The value of *(p+3) is "<<*(p+3)<<endl;
-    
-    
-   
     return 0;
 }
